a0a5771c2f046fac833db1f39a19eb0352795e5b
Mondragon <fimv705@gmail.com>
2023-08-02 02:40:04 -0600

    Tried 3x3 system of ODEs, boundary condition issue
    There's an issue in which the boundary condition is on the first derivative, which I haven't manage to implement

--- general/main.py
+++ general/main.py
@@ -5,15 +5,15 @@ from scipy.optimize import newton
 
 
 def yf(v0):
-    sol = solve_ivp(F, [x0, xf], [y0, v0])
-    y, v = sol.y
-    return y[-1] - y2
+    sol = solve_ivp(F, [x0, xf], [y0, w0, v0])
+    y, w, v = sol.y
+    return y[-1] - 1
 
 
 # ----------------------- Equations to solve -----------------------
 # s = [y(x)
 #      v(x)]
-F = lambda x, s: np.dot(np.array([[0, 1], [-3 / x ** 2, 3 / x + (24 * x ** 3) / s[1]]]), s)
+F = lambda x, s: np.dot(np.array([[0, 1, 0], [0, 0, 1], [-2*s[2], s[1], -1/s[2]]]), s)
 
 
 def aSol(r):  # analytical solution
@@ -21,23 +21,24 @@ def aSol(r):  # analytical solution
 
 
 # ----------------------- Boundary conditions -----------------------
-x0 = 1.
+x0 = 0.
 y0 = 0.  # Initial condition
-xf = 2.
-y2 = 0.
+w0 = 0.
+xf = 4.4
+w2 = 1.
 x_eval = np.linspace(x0, xf, 100)  # x range of evaluation, taking 100 subdivisions
 v0 = newton(yf, 8)  # Initial guess = 8, we use Newton method as Shooting
 print(v0)
 
 # ----------------------- Solving equations with RK45 -----------------------
-sol = solve_ivp(F, [x0, xf], [y0, v0], t_eval=x_eval)  # This uses RK45 to numerically integrate
+sol = solve_ivp(F, [x0, xf], [y0, w0, v0], t_eval=x_eval)  # This uses RK45 to numerically integrate
 
 # ----------------------- Plotting -----------------------
 plt.style.use("dark_background")
 plt.figure(figsize=(10, 8))
-plt.plot(sol.t, sol.y[0], 'bo')  # sol.t returns x values, sol.y returns solution values
-plt.plot(xf, y2, "ro")
-plt.plot(x_eval, aSol(x_eval), 'w')
+plt.plot(sol.t, sol.y[1], 'bo')  # sol.t returns x values, sol.y returns solution values
+plt.plot(xf, w2, "ro")
+# plt.plot(x_eval, aSol(x_eval), 'w')
 plt.xlabel("x")
 plt.ylabel("y")
 plt.title(f"root finding v={v0} m/s")
